b089907bc56a526575ef96d495f125b62c53b8c8
Slava <ishchenkoviacheslav@gmail.com>
2019-08-09 16:43:55 +0300

    its work!

--- aforgeTest/Program.cs
+++ aforgeTest/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 
@@ -26,84 +27,26 @@ namespace aforgeTest
 
             Bitmap pattern = new Bitmap(@"C:\Users\Slava\Downloads\garagePattern.png");
             //you should use the ToArgb method to compare two Colors
+
+            float[] sizes = new float[] { 0.5f, 0.75f, 1f, 1.25f, 1.5f };
+            List<Bitmap> patternSizes = new List<Bitmap>();
+            for (int i = 0; i < sizes.Length; i++)
+            {
+                Bitmap btm = new Bitmap((int)(pattern.Width * sizes[i]), (int)(pattern.Height * sizes[i]));
+                patternSizes.Add(btm);
+                Graphics graphics = Graphics.FromImage(btm);
+                graphics.DrawImage(pattern, 0, 0, btm.Width, btm.Height);
+                //pattern05.Save("05.png", ImageFormat.Png);
+            }
+
+
+
             int currentAssesment = 0;
             int bestAssesment = 0;
             int bestStartX = 0;
             int bestStartY = 0;
             int inc = 10;
-            //prepare pattern bitmap
-            //get and save different sizes
-            int[,] originalPattern = new int[pattern.Width, pattern.Height];
-            for (int w = 0; w < pattern.Width; w++)
-            {
-                for (int h = 0; h < pattern.Height; h++)
-                {
-                    originalPattern[w,h] = pattern.GetPixel(w, h).ToArgb();
-                }
-            }
-            float[] sizes = new float[] { 0.5f, 0.75f, 1f, 1.25f, 1.5f };
-            int[,] size05 = new int[(int)(originalPattern.GetLength(0) * sizes[0]),(int)(originalPattern.GetLength(1) * sizes[0])];
-            int[,] size075 = new int[(int)(originalPattern.GetLength(0) * sizes[1]),(int)(originalPattern.GetLength(1) * sizes[1])];
-            int[,] size1 = new int[(int)(originalPattern.GetLength(0) * sizes[2]),(int)(originalPattern.GetLength(1) * sizes[2])];
-            int[,] size125 = new int[(int)(originalPattern.GetLength(0) * sizes[3]),(int)(originalPattern.GetLength(1) * sizes[3])];
-            int[,] size15 = new int[(int)(originalPattern.GetLength(0) * sizes[4]),(int)(originalPattern.GetLength(1) * sizes[4])];
-            List<int[,]> allSizes = new List<int[,]>() { size05, size075, size1, size125, size15 };
-            Queue<NextPoint> allPoints = new Queue<NextPoint>();
-            for (int i = 0; i < sizes.Length; i++)
-            {
-                bool firstIteration = true;
-                int nextPixel = 0;
-                int counter = 0;
-
-                //increase
-                for (int w = 0; w < originalPattern.GetLength(0); w++)
-                {
-                    for (int h = 0; h < originalPattern.GetLength(1); h++)
-                    {
-                        if (firstIteration == true)
-                        {
-                            firstIteration = false;
-                            nextPixel = originalPattern[w, h];
-                            counter++;
-                        }
-                        else
-                        {
-                            if(nextPixel != originalPattern[w,h])
-                            {
-                                allPoints.Enqueue(new NextPoint() { ColorOfPixel = nextPixel, Counter = counter });
-                                counter = 1;
-                                nextPixel = originalPattern[w, h];
-                            }
-                            else
-                            {
-                                counter++;
-                            }
-                        }
-                    }
-                    bool coundition = true;
-                    for (int filler = 0; coundition ;filler++)
-                    {
-                        if(filler < (int)(allPoints.Peek().Counter * sizes[i]))
-                        {
-                            allSizes[i][w, filler] = allPoints.Peek().ColorOfPixel;
-                        }
-                        else
-                        {
-                            try
-                            {
-                                allPoints.Dequeue();
-                                allPoints.Peek();
-                            }
-                            catch (InvalidOperationException ex)
-                            {
-                                coundition = false;
-                            }
-                        }
-                        //заполнить allSizes теми же пикселями что и в allPoints, но только в пропорциях (для allSizes[i])
-                    }
-                    firstIteration = true;
-                }
-            }
+           
             //move pattern square inside image square
             for (int i = 0; i < 2; i++)
             {
